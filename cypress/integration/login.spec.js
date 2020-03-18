61be1c63a5ce04643a282d2b39b497569a5f9170
Daniel Pomfret <daniel.pomfet@working-age.digital.dwp.gov.uk>
2020-03-18 18:06:27 +0000

    Allow spaces in name

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -29,4 +29,18 @@ context('Actions', () => {
     cy.get('body').should('contain', 'Hello Scout!');
     cy.get('body').should('contain', 'Here is a list of Scout Badges you can work towards online...');
   });
+
+  it('Should not allow names that are not letters or numbers', () => {
+    cy.get('#scouts').click();
+    cy.get('#inputName').type('Scout&&&&&').should('have.value', 'Scout&&&&&');
+    cy.get('#loginSubmit').click();
+    cy.get('#alerts').should('contain', 'Name can only contain letters, numbers and spaces.');
+  });
+
+  it('Should allow names with spaces', () => {
+    cy.get('#scouts').click();
+    cy.get('#inputName').type('Scout One').should('have.value', 'Scout One');
+    cy.get('#loginSubmit').click();
+    cy.get('body').should('contain', 'Hello Scout One!');
+  });
 });
