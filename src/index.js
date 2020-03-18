cd36d76397a4318fa047f584e0784de5089986cf
Daniel Pomfret <daniel.pomfet@working-age.digital.dwp.gov.uk>
2020-03-18 17:36:30 +0000

    Allow configurable port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const pjson = require('../package.json');
 const { log } = console;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 nunjucks.configure(path.join(__dirname, 'views/'), {
   autoescape: true,
